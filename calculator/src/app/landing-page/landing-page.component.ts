bdf30e8479f2fdafec26958cec33b4568c68de49
ashishdhodia <ashishdhodia@gmail.com>
2022-03-21 12:00:37 +0530

    Calculator application done.

---
+++ calculator/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  input: string = '';
+  result: string = '';
+  flag = true;
+
+
+  clickNum(num: string) {
+    if (this.input == "Cannot divide by zero!"){
+      this.input = "";
+    }
+    if (num == "0") {
+      const previousChar = this.input[this.input.length - 1];
+      if (previousChar === '/') {
+        this.input = "Cannot divide by zero!";
+        return;
+      }
+    }
+
+    if (num == ".") {
+      const previousChar = this.input[this.input.length - 1];
+      if (previousChar === '.') {
+        return;
+      }
+    }
+    this.flag = false;
+    this.input = this.input + num
+    this.calcAnswer();
+  }
+
+  clickOperator(op: string) {
+    const endChar = this.input[this.input.length - 1];
+    if (endChar === '/' || endChar === '*' || endChar === '-' || endChar === '+') {
+      return;
+    }
+    this.input = this.input + op
+    this.calcAnswer();
+  }
+
+
+  clear() {
+    // console.log("click");
+    
+    if (this.input == "Cannot divide by zero!"){
+      this.input = "";
+    }
+    if (this.input != "") {
+      this.input = this.input.substring(0, this.input.length - 1)
+    }
+    if (this.input == "") {
+      this.result = "";
+      this.flag = true;
+    }
+  }
+
+  allClear() {
+    this.result = '';
+    this.input = '';
+    this.flag = true;
+  }
+
+  calcAnswer() {
+    let expString = this.input;
+    let endChar = expString[expString.length - 1];
+    if (endChar === '.') {
+      expString = expString.substring(0, expString.length - 1);
+    }
+    endChar = expString[expString.length - 1];
+    if (endChar === '/' || endChar === '*' || endChar === '-' || endChar === '+' || endChar === '.') {
+      expString = expString.substring(0, expString.length - 1);
+    }
+    // console.log("expString " + expString);
+    this.result = eval(expString);
+  }
+
+  getAnswer() {
+    this.calcAnswer();
+    this.input = this.result;
+    if (this.input == "0") {
+      this.input = "";
+      this.flag = true;
+    }
+  }
+
+
+
+  ngOnInit(): void {
+  }
+
+}
